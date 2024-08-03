880aa8fa6230c288198419ab23daf21baa1662a2
MaccSob <71sobmaciej@gmail.com>
2024-08-04 00:11:20 +0200

    testing testing

---
+++ my-first-react-app/src/Favorites.jsx
@@ -0,0 +1,8 @@
+function Favorite() {
+    return <ul>
+    <li>Chicken Sandwich</li>
+    <li>Steak</li>
+    <li>Fajitas</li>
+    <li>Tortillas but with turkey cuz healthier</li></ul>
+}
+export default Favorite;
--- my-first-react-app/src/main.jsx
+++ my-first-react-app/src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Favorite from './Favorites.jsx'
 import Greeting from './Greeting.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Greeting />
+    <Favorite></Favorite>
   </React.StrictMode>,
 )
