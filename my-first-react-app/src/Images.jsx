036ba886f9c456b3520317cdba5b9cee0ef8dfe9
MaccSob <71sobmaciej@gmail.com>
2024-08-04 10:36:01 +0200

    Learning 1234

---
+++ my-first-react-app/src/Images.jsx
@@ -0,0 +1,22 @@
+function Images() {
+    return  (
+
+        <>
+        <h1>Hedy Lamarr&quot;s&quot; Todos</h1>
+        <img 
+          src="https://i.imgur.com/4WnWwiy.jpeg" 
+          alt="Hedy Lamarr" 
+          className="photo"
+        />
+            <ul style={{
+      backgroundColor: 'white',
+      color: 'green'
+    }}>
+          <li>Invent new traffic lights</li>
+          <li>Rehearse a movie scene</li>
+          <li>Improve the spectrum technology</li>
+        </ul>
+      </>
+    )
+    }
+export default Images;
--- my-first-react-app/src/main.jsx
+++ my-first-react-app/src/main.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Images from './Images.jsx'
 import Favorite from './Favorites.jsx'
 import Greeting from './Greeting.jsx'
+import Avatar from './Avatar.jsx'
+import Bio from './Bio.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Bio></Bio>
     <Greeting />
     <Favorite></Favorite>
+    <Images></Images>
+    <Avatar></Avatar>
   </React.StrictMode>,
 )
